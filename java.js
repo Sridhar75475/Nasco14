45983f4fc3bd56fec0cb1d19141dd94e48b293e5
Sridhar75475 <97395431+Sridhar75475@users.noreply.github.com>
2022-02-18 01:07:30 +0530

    added new file

---
+++ java.js
@@ -0,0 +1,9 @@
+var valueToprint = "";  
+for(var i = 1; i <=5; i++) {
+        for(var j= 1; j <=i; j++){
+          valueToprint += i;
+        }
+        valueToprint += "\n";
+
+}
+console.log(valueToprint);
